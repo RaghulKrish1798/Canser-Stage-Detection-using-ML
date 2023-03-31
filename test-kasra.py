83699f78a2ef0c3be20ae5c8ae270971bbd0e1ec
Kasra Borazjani <kasraborazjani@Kasras-MacBook-Air.local>
2023-03-31 15:46:07 -0400

    initial commit on personal branch

--- test-kasra.py
+++ test-kasra.py
@@ -1,3 +1,5 @@
+### PURE FAILURE ###
+
 import openslide
 import os
 from glob import glob
@@ -15,3 +17,6 @@ test_region = test_Slide.read_region((30000,10000), 0, (300,300))
 
 blue_hist = cv2.calcHist(test_region, [0], None, [100], [0, 256])
 plt.hist(blue_hist, color="blue")
+
+
+print ("hey!")
