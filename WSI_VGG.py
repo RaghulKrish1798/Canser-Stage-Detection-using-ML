e03e0d3eeddf47cacdebaa2d4c307301eb3f58dc
RaghulKrish <raghulkrish1798@gmail.com>
2023-04-22 17:13:01 -0400

    VGG Feature Extraction and K-means with k = 10

--- WSI_VGG.py
+++ WSI_VGG.py
@@ -1,31 +1,51 @@
 import torchvision.models as models
 import torch
 from torchvision import transforms
-from torchvision.datasets import DatasetFolder
-from torch import nn
-import torchvision
-import cv2
 import numpy as np
+import os
 from PIL import Image
+from sklearn.cluster import KMeans
 
 # https://github.com/uta-smile/DeepAttnMISL/blob/master/DeepAttnMISL_model.py
 
-# Downloading the pretrained VGG-16 Model
-model = models.vgg16(pretrained=True)
+def VGG16_feature_extractor(data):
+    # Downloading the pretrained VGG-16 Model
+    model = models.vgg16(pretrained=True)
+    transform = transforms.Compose([
+        transforms.Resize(224),  # resize the image to 224x224
+        transforms.ToTensor(),   # convert the image to a PyTorch tensor
+    ])
+    
+    data = [transform(image) for image in data]
+    data = torch.stack(data)
+    # data = data.unsqueeze(0)
+    print(data.shape)
+    model.eval()
+    with torch.no_grad():
+        forward = model(data)
+        print(forward.shape)
+    return forward
 
-# Loading the Patches
-data_path = "T:\CV\EXtracted_Patches\TCGA-A1-A0SF-01A\patch_1084.png"
+def K_means_clustering(feature):
+    feature = np.array(feature)
+    k = 10
+    kmeans = KMeans(n_clusters=k, init='k-means++', max_iter=300, n_init=10, random_state=0)
+    kmeans.fit(feature)
+    cluster_assignments = kmeans.labels_
+    print(cluster_assignments.shape)
+    print(cluster_assignments)
+    return cluster_assignments
 
-transform = transforms.Compose([
-    transforms.Resize(224),  # resize the image to 224x224
-    transforms.ToTensor(),   # convert the image to a PyTorch tensor
-])
+def load_images():
+    # Loading the Patches
+    data_path = "T:\CV\EXtracted_Patches\Test"
+    img_name = os.listdir(data_path)
+    images = []
+    for file_name in img_name:
+        image = Image.open(os.path.join(data_path, file_name))
+        images.append(image)
+    return images
 
-data = Image.open(data_path)
-data = transform(data)
-data = data.unsqueeze(0)
-print(data.shape)
-model.eval()
-with torch.no_grad():
-    forward = model(data)
-    print(forward.shape)
+patches = load_images() # Loading the Extracted Patches
+features = VGG16_feature_extractor(patches) # Extracting Features
+cluster = K_means_clustering(features)  # Performing K-means Clustering on the extracted features
